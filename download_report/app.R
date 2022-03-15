fc3b4c351e19f62bf6429979ffa606776d73c4a6
Md Abu Yusuf Ansari <mansari@umc.edu>
2022-03-15 10:20:32 -0500

    Trying to generate report. source: https://shiny.rstudio.com/articles/generating-reports.html

--- download_report/app.R
+++ download_report/app.R
@@ -10,112 +10,37 @@
 library(shiny)
 library(tidyverse)
 
-# Define UI 
-ui <- fluidPage(
-    
-    sidebarPanel(
-        
-        selectizeInput("select",
-                       label = "Cylinder" ,
-                       choices = unique(mtcars$cyl),
-                       selected = 4
-        ),
-        
-
-        
-        
-        
-        downloadButton("report", "Generate report for selected"),
-        br(),br()
-    ),
-    
-    mainPanel(
-        DT::dataTableOutput('myTable1')
-    )
-
-
+ui = fluidPage(
+    sliderInput("slider", "Slider", 1, 100, 50),
+    downloadButton("report", "Generate report")
 )
 
-# Define server 
-server <- function(input, output) {
-    
-    mtcars <- tibble::rownames_to_column(mtcars, "Name")
-    v <- reactiveValues(data = NULL)
-    
-    observeEvent(input$select, {
-        v$data <- mtcars %>% filter(cyl == input$select)
-        write.csv(v$data, "mydata.csv")
-    })
-    
-    
-    output$myTable1 <- DT::renderDataTable({
-        
-        if (is.null(v$data)) return()
-        DT::datatable(v$data) 
-    })
-    
-    output$report <- downloadHandler(
-        
-        filename = function(){
-            paste0(v$data$Name[input$myTable1_rows_selected],".doc")
-        },
-        content = function(file) {
-            params = list(j=input$myTable1_rows_selected)
-            p1 = here::here("download_report", "report.Rmd")
-            p2 = here::here("download_report", "New Microsoft Word Document.docx")
-            src = c(p1, p2)
-            
-            owd <- setwd(tempdir())
-            on.exit(setwd(owd), add = TRUE)
-            file.copy(from = src, to = c('report.Rmd', 'New Microsoft Word Document.docx'), overwrite = TRUE)
-            # browser()
-            # k <- (input$myTable1_rows_selected)
-            # fs <- c()
-            # for ( i in k) 
-            # {
-                # path <- paste0(v$data$Name[k],".doc")
 
-                
-                rmarkdown::render(here::here("download_report", "report.Rmd"), output_file = file,
-                                  params = params,
-                                  envir = new.env(parent = globalenv())
-                )
-            #     fs <- c(fs,path)
-            #     
-            # # }
-            # zip(file,fs)
+server = function(input, output) {
+    output$report <- downloadHandler(
+        # For PDF output, change this to "report.pdf"
+        filename = "report.html",
+        content = function(file) {
+            # Copy the report file to a temporary directory before processing it, in
+            # case we don't have write permissions to the current working dir (which
+            # can happen when deployed).
+            tempReport <- file.path(tempdir(), "report.Rmd")
+            file.copy("report.Rmd", tempReport, overwrite = TRUE)
+            
+            # Set up parameters to pass to Rmd document
+            params <- list(n = input$slider)
+            
+            # Knit the document, passing in the `params` list, and eval it in a
+            # child of the global environment (this isolates the code in the document
+            # from the code in this app).
+            rmarkdown::render(tempReport, output_file = file,
+                              params = params,
+                              envir = new.env(parent = globalenv())
+            )
         }
     )
-    
-    
-    # output$report <- downloadHandler(
-    #     
-    #     filename = function(){
-    #         paste0(input$select,".doc")
-    #     },
-    #     content = function(file) {
-    #         params = list(j = input$select)
-    #         
-    #         p1 = "C:\\Users\\mansari\\University of Mississippi Medical Center\\Pramod Anugu - Exam 4\\Weekly Reports\\Other Reports\\Blood Pressure\\report.Rmd"
-    #         p2 = "C:\\Users\\mansari\\University of Mississippi Medical Center\\Pramod Anugu - Exam 4\\Weekly Reports\\Other Reports\\Blood Pressure\\CARDIA EXAM X.docx"
-    #         src = c(p1, p2)
-    #         
-    #         owd <- setwd(tempdir())
-    #         on.exit(setwd(owd), add = TRUE)
-    #         file.copy(from = src, to = c('report.Rmd', 'CARDIA EXAM X.docx'), overwrite = TRUE)
-    #         
-    #         rmarkdown::render('report.Rmd', output_file = file,
-    #                           params = params,
-    #                           envir = new.env(parent = globalenv())
-    #         )
-    #         
-    #         
-    #         
-    #     }
-    # )
-
-
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
